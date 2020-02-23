9d12705bd292ed1740af119257f7a6b78f78c1fb
gontse <gontse@optiflex.co.za>
2020-02-23 03:28:27 +0200

    Added jpa details in propertise file, created 2 models, 1)User, 2) Group and created the repository interfaces

--- src/main/java/com/optiflex/model/UserModel.java
+++ src/main/java/com/optiflex/model/UserModel.java
@@ -1,5 +1,78 @@
 package com.optiflex.model;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="optiusers")
 public class UserModel {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String username;
+	private String surname;
+	private String nickname;
+	private String role;
+	private String pass;
+	private String email;
+	@OneToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="groupid", referencedColumnName="id")
+	private GroupModel groupmodel;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public GroupModel getGroupmodel() {
+		return groupmodel;
+	}
+	public void setGroupmodel(GroupModel groupmodel) {
+		this.groupmodel = groupmodel;
+	}
+	
+	
 }
