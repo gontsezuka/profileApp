decff70ad64799f80e5d34bc6ffb5a826246b20d
gontse <gontse@optiflex.co.za>
2020-02-24 14:44:15 +0200

    The first ever test works here for the UserService test, but i still have to provide coverage information on the test and code

--- src/test/java/com/optiflex/service/UserServiceTests.java
+++ src/test/java/com/optiflex/service/UserServiceTests.java
@@ -1,5 +1,35 @@
 package com.optiflex.service;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.optiflex.model.UserModel;
+import com.optiflex.repository.UserRepository;
+
+@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest
 public class UserServiceTests {
 
+	@InjectMocks
+	private UserService userService;
+	@Mock
+	private UserRepository userRepository;
+	
+	
+	@Test
+	public void getAllUsersTest()
+	{
+		when(userRepository.findAll()).thenReturn(Stream.of(new UserModel(),new UserModel()).collect(Collectors.toList()));
+		assertEquals(2,userService.getAllUsers().size());
+	}
 }
